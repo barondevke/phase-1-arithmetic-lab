15adba0c5650108ee96ff8decfd1ee8c74025154
Steve Baron <ndabasteve1@gmail.com>
2023-06-12 14:44:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10)
+
+const num3 = 10
+const num4 = 6
+
+let mod = num3 % num4
+
+let max = Math.max(1, 8, 2, 20)
